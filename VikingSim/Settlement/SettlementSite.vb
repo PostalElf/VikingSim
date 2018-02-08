9da1af7db913117d49ec6e943323e2c1eae39298
PostalElf <thanthos@gmail.com>
2018-02-08 15:03:37 +0800

    Updated SettlementSite.Construct
    Terrain now determines 2 (or more) guaranteed locations

--- VikingSim/Settlement/SettlementSite.vb
+++ VikingSim/Settlement/SettlementSite.vb
@@ -1,25 +1,37 @@
 ﻿Public Class SettlementSite
     Public Terrain As String
     Public LocationList As New List(Of String)
-    Public Shared Function Construct(Optional ByVal locations As List(Of String) = Nothing) As SettlementSite
+    Public Shared Function Construct(Optional ByVal terrain As String = Nothing) As SettlementSite
+        'allLocation holds a list of all the different types of locations
+        Dim terrainDict As Dictionary(Of String, List(Of String)) = IO.ImportSquareBracketList(IO.sbTerrain)
+        Dim allLocation As New List(Of String)
+        For Each k In terrainDict.Keys
+            For Each l In terrainDict(k)
+                If allLocation.Contains(l) = False Then allLocation.Add(l)
+            Next
+        Next
+
         Dim site As New SettlementSite
         With site
-            If locations Is Nothing Then
-                Dim locationDict As Dictionary(Of String, List(Of String)) = IO.ImportSquareBracketList(IO.sbTerrain)
-                Dim locationKeys As New List(Of String)
-                For Each k In locationDict.Keys
-                    locationKeys.Add(k)
-                Next
-
-                'roll 2-5 locations
-                Dim numLocations As Integer = rng.Next(2, 6)
-                For n = 1 To numLocations
-                    Dim location As String = GetRandom(Of String)(locationKeys)
-                    .LocationList.Add(location)
-                Next
+            If terrain = "" Then
+                Dim terrainRoll As Integer = rng.Next(terrainDict.Keys.Count)
+                .Terrain = terrainDict.Keys(terrainRoll)
             Else
-                .LocationList.AddRange(locations)
+                .Terrain = terrain
             End If
+            Dim terrainList As List(Of String) = terrainDict(.Terrain)
+
+            'roll number of locations (2-7)
+            Dim numLocations As Integer = rng.Next(2, 8)
+            For n = 1 To numLocations
+                If n - 1 <= terrainList.Count - 1 Then
+                    'get guaranteed location from terrainList
+                    .LocationList.Add(terrainList(n - 1))
+                Else
+                    'roll random location
+                    .LocationList.Add(GetRandom(allLocation))
+                End If
+            Next
         End With
         Return site
     End Function
